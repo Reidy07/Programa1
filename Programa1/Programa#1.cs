6b358b2c17c5d0ea394b2cd73b4918b187735fd8
Reidy07 <reidybrown1234@gmail.com>
2024-05-09 08:52:29 -0400

    Agregar archivos de proyecto.

---
+++ Programa1/Programa#1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Programa1
+{
+    public class Programs
+    {
+        public static void Main(string[] args)
+        {
+            double lado;
+            double perimetro;
+
+            try
+            {
+                Console.WriteLine("Ingresar la longitud del lado del cuadrado: ");
+                lado = Convert.ToDouble(Console.ReadLine());
+
+                perimetro = lado * 4;
+                Console.WriteLine("El perimetro del cuadrado es :" + perimetro);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"El programa dejo de funcionar por esta razon: {ex.Message}");
+             
+            }
+        }
+    }
+}
